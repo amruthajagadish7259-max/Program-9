eea539623c18773ade2b75e0b12ced8c2c8d8a31
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-12-05 20:30:20 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,9 +1,9 @@
 from product import product_details
 def test_product_details():
-  expected_output=(
+  expected_output = (
     "Product ID:P100\n"
     "Name:Samsung S20 FE 5G\n"
     "Quantity:1\n"
     "Price:28000"
   )
-  assert product_details("P100e","Samsung S20 FE 5G",1,28000)==expected_output
+  assert product_details("P100", "Samsung S20 FE 5G", 1, 28000) == expected_output

00e36747e7584d2341c5c7b932029e8c373dea94
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-12-05 20:53:38 +0530

    Update product.py

--- product.py
+++ product.py
@@ -1,16 +1,16 @@
 def product_details(p_id, name, qty, price):
    result = (
-      f"Product ID: {p_id}\n"
-      f"Name: {name}\n"
-      f"Quantity: {qty}\n"
-      f"Price: {price}"
+      f"Product ID:{p_id}\n"
+      f"Name:{name}\n"
+      f"Quantity:{qty}\n"
+      f"Price:{price}"
    )
    return result
    
-   if __name__ == "__main__":
-      # sample output (you can change)
-      p_id = "P100"
-      name = "Samsung S20 FE 5G"
-      qty = 1
-      price = 28000
-      print(product_details(p_id, name, qty, price))
+if __name__ == "__main__":
+   # sample output (you can change)
+   p_id="P100"
+   name="Samsung S20 FE 5G"
+   qty=1
+   price=28000
+   print(product_details(p_id, name, qty, price))

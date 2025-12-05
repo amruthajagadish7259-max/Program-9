c1626333189472979d07627f5a708f6d7c01f912
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-12-05 20:18:13 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -3,7 +3,7 @@ def test_product_details():
   expected_output=(
     "Product ID:P100\n"
     "Name:Samsung S20 FE 5G\n"
-    "Quntity:1\n"
+    "Quantity:1\n"
     "Price:28000"
   )
   assert product_details("P100e","Samsung S20 FE 5G",1,28000)==expected_output
